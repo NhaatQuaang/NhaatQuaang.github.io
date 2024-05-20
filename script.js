ac09a04f7386d04076842dca9cde9688d38341b1
gnauQ <165934901+NhaatQuaang@users.noreply.github.com>
2024-05-21 00:51:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Set the date we're counting down to
-//var countDownDate = new Date("Sep 23, 2024 23:30:00").getTime();
-var countDownDate = new Date("Aug 13, 2024 00:02:00").getTime();
+var countDownDate = new Date("Sep 24, 2024 00:00:00").getTime();
+//var countDownDate = new Date("Aug 13, 2024 00:02:00").getTime();
 
 // Update the count down every 1 second
 var countdownfunction = setInterval(function() {
