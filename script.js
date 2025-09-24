b1e3d53a18c51188effc3b548eb425d974aee2d3
gnauQ <165934901+NhaatQuaang@users.noreply.github.com>
2025-09-24 17:01:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -202,3 +202,32 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 });
+// Set the date we're counting down to
+//var countDownDate = new Date("Sep 24, 2024 00:00:00").getTime();
+var countDownDate = new Date("Mar 17, 2025 11:00:00").getTime();
+
+// Update the count down every 1 second
+var countdownfunction = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now an the count down date
+    var distance = now - countDownDate;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = days + " ngày  " + hours + " giờ  " +
+        minutes + " phút  " + seconds + " giây  ";
+
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+        document.getElementBy("lmao").innerHTML = "Happy Birthday";
+    }
+}, 1000);
